85d4b307ac52fa4272c74cce1de6cb7621d126d9
Janengethe <janengethew@gmail.com>
2021-04-14 18:01:10 +0000

    checked with betty

--- builtin_launch.c
+++ builtin_launch.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
+
 /**
  *shell_help - implements builtin help
  *@args:array of strings
  *Return:1
  */
-/*
 int shell_help(char **args)
 {
 void(args);
@@ -13,7 +13,8 @@ printf("Type program names and their arguements then press enter\n");
 printf("Builtin commands:/n cd/nhelp/n/exit/n");
 printf("Explore the manpage to know about other programs\n");
 return (1);
-}*/
+}
+
 /**
  *shell_environ - prints environment variables when env is invoked
  *
@@ -21,11 +22,10 @@ return (1);
  */
 int shell_environ(void)
 {
-  int i = 0;
-
-  for (i = 0; environ[i]; i++)
-    {
-      printf("%s\n", environ[i]);
-    }
-  return (1);
+int i = 0;
+for (i = 0; environ[i]; i++)
+{
+printf("%s\n", environ[i]);
+}
+return (1);
 }
