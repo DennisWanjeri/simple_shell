2153f980ab74a798a2828d11875d7f8fbc11ebd5
DennisWanjeri <shikandadennis07@gmail.com>
2021-04-15 14:17:06 +0000

    merged

--- read_line.c
+++ read_line.c
@@ -71,3 +71,26 @@ void free_args(char **args)
 	}
 	free(args);
 }
+include "holberton.h"
+/**
+ *read_line - reads user's input from stdin
+ *
+ *Return:pointer to string containing user input
+ */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+
+	if (getline(&line, &len, stdin) == 0)
+	{
+		if (feof(stdin))
+		{
+			free(line);
+			exit(EXIT_SUCCESS);
+		}
+		free(line);
+		exit(EXIT_FAILURE);
+	}
+	return (line);
+}
